67c5d5e579b2369b8c38b009f41c1bb244c9f8d4
unknown <abhi.codeforfun777@gmail.com>
2019-10-24 10:18:46 +0530

    how to insert image

---
+++ db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `uploadedimage` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `imagename` varchar(100) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
